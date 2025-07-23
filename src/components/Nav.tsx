d3d61dd69344fce5430b232e319160a395f1b533
David Soldat <davidsoldat00@gmail.com>
2025-07-23 21:14:02 +0200

    fixing nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,13 +5,13 @@ export default function Nav() {
   const companyName = company ? JSON.parse(company).name : "Company";
   return (
     <nav className="w-full bg-gray-100 px-4 py-6 drop-shadow-md md:px-5 md:py-10">
-      <div className="grid grid-cols-3 items-center">
-        <div className="flex justify-start"></div>
+      <div className="grid grid-cols-2 items-center lg:grid-cols-3">
+        <div className="hidden justify-start lg:flex"></div>
 
         <div className="flex items-center justify-center">
           <Link
             to="/dashboard"
-            className="text-center text-xl font-bold text-sky-800 drop-shadow-sm sm:text-2xl md:text-3xl"
+            className="text-center text-xl font-bold text-nowrap text-sky-800 drop-shadow-sm sm:text-2xl md:text-3xl"
           >
             The Payments
           </Link>
@@ -20,9 +20,9 @@ export default function Nav() {
         <div className="flex items-center justify-end">
           <Link
             to="/about"
-            className="text-right text-sm font-normal text-sky-700 sm:text-base md:text-xl"
+            className="text-right text-sm font-normal text-nowrap text-sky-700 sm:text-base md:text-xl"
           >
-            <span className="hidden sm:inline">{companyName}</span>
+            <span className="">{companyName}</span>
           </Link>
         </div>
       </div>
